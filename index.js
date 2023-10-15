8105d0cb7eb1219d5a54c43f01da2480d24a6e9e
joey <joey_qpb@hotmail.com>
2023-10-15 23:23:13 +1100

    imnplements floorCollisions

--- index.js
+++ index.js
@@ -10,47 +10,25 @@ const scaledCanvas = {
 }
 const gravity = 0.5;
 
-class Sprite {
-    constructor({ position, imageSrc }) {
-        this.position = position;
-        this.image = new Image();
-        this.image.src = imageSrc;
-    }
-
-    draw() {
-        if (!this.image) return
-        c.drawImage(this.image, this.position.x, this.position.y);
-    }
-
-    update() {
-        this.draw();
-    }
+//2d array
+const floorCollisions2D = [];
+for (let i = 0; i < floorCollisions.length; i += 36) {
+    floorCollisions2D.push(floorCollisions.slice(i, i + 36));
 }
 
-class Player {
-    constructor(position, height) {
-        this.position = position;
-        this.velocity = {
-            x: 0,
-            y: 1
+const collisionsBlocks = []
+floorCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol == 202) {
+            collisionsBlocks.push(new CollisionsBlocks({
+                position: {
+                    x: x * 16,
+                    y: y * 16
+                }
+            }))
         }
-        this.height = 100;
-    }
-
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 100, this.height);
-    }
-
-    update() {
-        this.draw();
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height + this.velocity.y < canvas.height) this.velocity.y += gravity;
-        else this.velocity.y = 0;
-    }
-}
+    })
+})
 
 const player = new Player({
     x: 0,
@@ -79,6 +57,9 @@ function animate() {
     c.scale(4, 4);
     c.translate(0, -background.image.height + scaledCanvas.height);
     background.update();
+    collisionsBlocks.forEach((collisionsBlock) => {
+        collisionsBlock.update();
+    })
     c.restore();
 
     player.update();
